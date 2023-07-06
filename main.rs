124e1f4e76d47bc343f1f08a167db9121aafd486
shimonfiddler <94184127+shimonfiddler@users.noreply.github.com>
2023-07-06 14:59:49 +0300

    diffrent types of print

--- main.rs
+++ main.rs
@@ -1,5 +1,8 @@
 fn main(){
+    print!("a single print");
+    print!("with another");
     println!("hello World");
+    println!("a seccond line");
     // this is a test comment
     /* this 
     is 
