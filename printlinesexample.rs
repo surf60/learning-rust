f1157550ec8199a041323fa12dd5b011caf52af0
shimonfiddler <94184127+shimonfiddler@users.noreply.github.com>
2023-07-06 15:02:37 +0300

    remaned main to pritlinesexample

---
+++ printlinesexample.rs
@@ -0,0 +1,13 @@
+fn main(){
+    print!("a single print");
+    print!("with another");
+    println!("hello World");
+    println!("a seccond line");
+    // this is a test comment
+    /* this 
+    is 
+    a 
+    block
+    comment
+    */
+}
