58159d7db2161d95268bfdfba03a5a263589ed1f
shimonfiddler <94184127+shimonfiddler@users.noreply.github.com>
2023-07-06 15:20:09 +0300

    made file for variable muability examples

---
+++ mutabiltiy.rs
@@ -0,0 +1,13 @@
+fn main(){
+        // variable to store integer value
+        let age = 31;
+        println!("Age: {}", age);
+    
+        // variable to store floating-point value
+        let salary = 342523.23;
+        println!("Salary: {}", salary);
+    
+        // variable to store string
+        let name = "Jackie";
+        println!("Name: {}", name);
+}
