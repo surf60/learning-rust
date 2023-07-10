4fa2d692f766fc310e46f956541f3030dd852b05
shimonfiddler <94184127+shimonfiddler@users.noreply.github.com>
2023-07-10 14:52:43 +0300

    unsigned example

--- src/datatypes.rs
+++ src/datatypes.rs
@@ -1,9 +1,13 @@
 fn main() {
     // signed integer type
-    let x: i32 = -200;
+    /*let x: i32 = -200;
     let y: i32 = 200;
 
     println!("x = {x}");
-    println!("y = {y}");
+    println!("y = {y}");*/
     
+    //unsigned integer type (positive only)
+    let x: u32 = 300;
+
+    println!("x = {x}");
 }
