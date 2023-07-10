bb145e2daabfd6621896409fd2c2dabeacc513a8
shimonfiddler <94184127+shimonfiddler@users.noreply.github.com>
2023-07-10 14:54:31 +0300

    added datatyes table

--- src/datatypes.rs
+++ src/datatypes.rs
@@ -1,4 +1,10 @@
 fn main() {
+    /*Size	Signed	Unsigned
+    8-bit	i8	u8
+    16-bit	i16	u16
+    32-bit	i32	u32
+    64-bit	i64	u64
+    128-bit	i128	u128*/
     // signed integer type
     /*let x: i32 = -200;
     let y: i32 = 200;
